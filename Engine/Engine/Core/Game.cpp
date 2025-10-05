7ff7af9d7893a173a6cad83c7153b8311b6645db
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2025-10-05 22:51:41 +0300

    new texture added

--- Engine/Engine/Core/Game.cpp
+++ Engine/Engine/Core/Game.cpp
@@ -32,13 +32,23 @@ bool Game::Init(const string& title, int width, int height)
 		return false;
 	}
 
-	m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED);
+	m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 	if (!m_Renderer)
 	{
 		cerr << "Renderer failed" << SDL_GetError() << endl;
 		return false;
 	}
+
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+
+	m_BrickTex = LoadTexture("Assets/Textures/brick.png");
+	if (!m_BrickTex)
+	{
+		return false;
+	}
+
+	SDL_QueryTexture(m_BrickTex, nullptr, nullptr, &m_BrickDst.w, &m_BrickDst.h);
 
 	m_isRunning = true;
 	return true;
@@ -91,13 +101,31 @@ void Game::ProcessInput()
 
 void Game::Update(float)
 {
+	m_BrickPos.x += m_BrickVel.x;
+	m_BrickPos.y += m_BrickVel.y;
 
+	if (m_BrickPos.x <0 || m_BrickPos.x + m_BrickDst.w > m_WindowWidth)
+	{
+		m_BrickVel.x *= -1;
+	}
+
+	if (m_BrickPos.y <0 || m_BrickPos.y + m_BrickDst.h > m_WindowHeight)
+	{
+		m_BrickVel.y *= -1;
+	}
 }
 
 void Game::Render()
 {
+
 	SDL_SetRenderDrawColor(m_Renderer, 30, 30, 30, 255);
 	SDL_RenderClear(m_Renderer);
+
+	m_BrickDst.x = static_cast<int> (m_BrickPos.x);
+	m_BrickDst.y = static_cast<int> (m_BrickPos.y);
+
+	SDL_RenderCopy(m_Renderer,m_BrickTex,nullptr,&m_BrickDst);
+
 	SDL_RenderPresent(m_Renderer);
 }
 
@@ -119,4 +147,20 @@ void Game::ShutDown()
 	}
 
 	SDL_Quit();
+}
+
+SDL_Texture* Game::LoadTexture(const string& path)
+{
+	SDL_Surface* surface = IMG_Load(path.c_str());
+
+	if (!surface)
+	{
+		cerr << IMG_GetError() << "\n";
+		return nullptr;
+	}
+
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(m_Renderer, surface);
+	SDL_FreeSurface(surface);
+
+	return tex;
 }
--- Engine/Engine/Core/Game.h
+++ Engine/Engine/Core/Game.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL2/SDL.H>
+#include <SDL2/SDL_image.h>
 #include <string>
 
 using namespace std;
@@ -22,6 +23,14 @@ private:
 private:
 	SDL_Window* m_Window;
 	SDL_Renderer* m_Renderer;
+	SDL_Texture* LoadTexture(const string& path);
+
+	struct Vec2 { float x{ 0.f }, y{ 0.f }; };
+	SDL_Texture* m_BrickTex = nullptr;
+	SDL_Rect m_BrickDst{ 0,0,0,0 };
+	Vec2 m_BrickPos{ 100.f, 100.f };
+	Vec2 m_BrickVel{ 2.f,1.f };
+
 	bool m_isRunning = false;
 	int m_WindowWidth = 800;
 	int m_WindowHeight = 600;
